c3c69f87dc364e1a13af0baa32662de06625a140
OksanaNevolina <onevolina5gmail.com>
2023-04-27 15:06:36 +0300

    task6 -index -style+@media

---
+++ additionalTasks/test_JS/scriptJS.js
@@ -0,0 +1,11 @@
+    let h1= document.querySelector('h1');
+    h1.innerText= 'Кролівництво';
+
+
+    let zxc = 'Кролівни́цтво — допоміжна галузь тваринництва, що забезпечує виробництво дієтичного хутра, пуху, шкіри, тощо при розведенні одомашненої форми виду кріль європейський.';
+     let p = document.createElement('p');
+     p.innerText = zxc;
+     document.body.appendChild(p);
+
+     let nameUser = document.querySelector(.nameUser);
+     fetch('')
